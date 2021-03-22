2a5f5996728234f4c614660db64052371691151e
RYUSEULGI <rhythm296@naver.com>
2021-03-22 18:22:54 +0900

    add front

---
+++ Aworkspace/fe/src/cmm/container/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    const formStyle = {
+        width: "500px",
+        margin: "0 auto",
+        paddingTop: "100px",
+        textAlign: "center"
+    }
+    return (
+        <div style={formStyle}>
+            <h1>Main</h1>
+            <Link to={"Signup"}>회원가입</Link><br/>
+            <Link to={"Login"}>로그인</Link><br/>
+            <Link to={"Counter"}>카운터</Link><br/>
+        </div>
+    )
+}
+
+export default Home
---
+++ Aworkspace/fe/src/uss/container/Signup.js
@@ -0,0 +1,62 @@
+import React from "react";
+import axios from "axios";
+import "./css/Signup.css";
+
+const Signup = () => {
+	const url = "#";
+	const insertMany = (e) => {
+		e.preventDefault();
+		alert("dummy data");
+		axios
+			.get("http://localhost:8080/auth/insertMany")
+			.then((res) => console.log(`${res.data.message} 등록됨`))
+			.catch((err) => console.log(err));
+	};
+
+	return (
+		<form style={{ border: "1px solid #ccc" }}>
+			<div className="container">
+				<h1>Sign Up</h1>
+				<p>Please fill in this form to create an account.</p>
+				<button onClick={insertMany}>input dummy data</button>
+				<hr />
+
+				<label htmlFor="email">
+					<b>Email</b>
+				</label>
+				<input type="text" placeholder="Enter Email" name="email" />
+
+				<label htmlFor="psw">
+					<b>Password</b>
+				</label>
+				<input type="password" placeholder="Enter Password" name="psw" />
+
+				<label htmlFor="psw-repeat">
+					<b>Repeat Password</b>
+				</label>
+				<input type="password" placeholder="Repeat Password" name="psw-repeat" />
+
+				{/* <label>
+                    <input type="checkbox" checked="checked" name="remember" style={{marginBottom: "15px"}}/> Remember me
+                </label> */}
+				<p>
+					By creating an account you agree to our{" "}
+					<a href={url} style={{ color: "dodgerblue" }}>
+						Terms & Privacy
+					</a>
+					.
+				</p>
+
+				<div className="clearfix">
+					<button type="button" className="cancelbtn">
+						Cancel
+					</button>
+					<button type="submit" className="signupbtn">
+						Sign Up
+					</button>
+				</div>
+			</div>
+		</form>
+	);
+};
+export default Signup;
